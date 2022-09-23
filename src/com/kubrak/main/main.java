f2da7b5442b58a4f04d026813e76c5a5cf9ae1b8
a.kubrak <padme@bbq.agency>
2022-09-23 20:13:22 +0300

    first test added

--- src/com/kubrak/main/main.java
+++
@@ -1,7 +0,0 @@
-package com.kubrak.main;
-
-public class main {
-    public static void main(String[] args) {
-        System.out.println("Hi!");
-    }
-}
---
+++ src/test/java/ua/ithillel/testng/FirstTest.java
@@ -0,0 +1,13 @@
+package ua.ithillel.testng;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class FirstTest {
+
+    @Test
+    public void testOne(){
+        Assert.assertTrue(true);
+
+    }
+}
