fa6793f0993169d8ee86f8b426dc0ed20bb184ab
a.kubrak <padme@bbq.agency>
2022-10-06 23:17:12 +0300

    Add RegistrationTest and LoginTest

---
+++ src/test/java/lesson16/LoginTest.java
@@ -0,0 +1,81 @@
+package lesson16;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import ui.browser.WebDriverFactory;
+import ui.utils.ConfigProvider;
+
+import java.time.Duration;
+import java.util.Random;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated;
+
+public class LoginTest {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @BeforeTest
+    public void start() {
+
+        driver = WebDriverFactory.getDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+    }
+
+    @Test
+    public void loginAsExistedUser() {
+
+        String username = generateRandomString();
+        String password = generateRandomString();
+
+        driver.get(ConfigProvider.SITE_URL);
+        driver.findElement(By.id("register")).click();
+        wait.until(visibilityOfElementLocated(By.id("register-modal")));
+        driver.findElement(By.id("register-username-modal")).sendKeys(username);
+        driver.findElement(By.id("register-first-modal")).sendKeys("firstname" + generateRandomString());
+        driver.findElement(By.id("register-last-modal")).sendKeys("lastname" + generateRandomString());
+        driver.findElement(By.id("register-email-modal")).sendKeys("testemail@" + generateRandomString() + "test.com");
+        driver.findElement(By.id("register-password-modal")).sendKeys(password);
+        driver.findElement(By.xpath("//*[@id=\"register-modal\"]/div/div/div[2]/form/p/button")).click();
+        wait.until(visibilityOfElementLocated(By.id("logout")));
+        driver.findElement(By.id("logout")).click();
+        wait.until(visibilityOfElementLocated(By.id("login")));
+
+        driver.findElement(By.id("login")).click();
+        wait.until(visibilityOfElementLocated(By.id("Login")));
+        driver.findElement(By.name("username")).sendKeys(username);
+        driver.findElement(By.id("password-modal")).sendKeys(password);
+        driver.findElement(By.xpath("//*[@id=\"login-modal\"]/div/div/div[2]/form/p/button")).click();
+        Assert.assertEquals(driver.findElement(By.id("login-message")).getAttribute("textContent"),"Login successful.");
+
+    }
+
+
+    @AfterTest
+    public void stop() {
+        driver.close();
+        driver.quit();
+    }
+    public String generateRandomString() {
+
+        Random rand = new Random();
+
+        String randString = rand.ints(48, 123)
+
+                .filter(num -> (num < 58 || num > 64) && (num < 91 || num > 96))
+
+                .limit(15)
+
+                .mapToObj(c -> (char) c).collect(StringBuffer::new, StringBuffer::append, StringBuffer::append)
+
+                .toString();
+        return randString;
+    }
+
+}
---
+++ src/test/java/lesson16/RegistrationTest.java
@@ -0,0 +1,67 @@
+package lesson16;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import ui.browser.WebDriverFactory;
+import ui.utils.ConfigProvider;
+
+import java.time.Duration;
+import java.util.Random;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated;
+
+
+public class RegistrationTest {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+
+
+    @BeforeTest
+    public void start() {
+
+        driver = WebDriverFactory.getDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    @Test
+    public void registerWithValidData() {
+        driver.get(ConfigProvider.SITE_URL);
+        driver.findElement(By.id("register")).click();
+        wait.until(visibilityOfElementLocated(By.id("register-modal")));
+        driver.findElement(By.id("register-username-modal")).sendKeys("username" + randomString);
+        driver.findElement(By.id("register-first-modal")).sendKeys("firstname" + randomString);
+        driver.findElement(By.id("register-last-modal")).sendKeys("lastname" + randomString);
+        driver.findElement(By.id("register-email-modal")).sendKeys("testemail@" + randomString + "test.com");
+        driver.findElement(By.id("register-password-modal")).sendKeys("password" + randomString);
+        driver.findElement(By.xpath("//*[@id=\"register-modal\"]/div/div/div[2]/form/p/button")).click();
+        Assert.assertEquals(driver.findElement(By.id("registration-message")).getAttribute("textContent"),"Registration and login successful.");
+    }
+
+    Random rand = new Random();
+
+    String randomString = rand.ints(48, 123)
+
+            .filter(num -> (num<58 || num>64) && (num<91 || num>96))
+
+            .limit(15)
+
+            .mapToObj(c -> (char)c).collect(StringBuffer::new, StringBuffer::append, StringBuffer::append)
+
+            .toString();
+
+    @AfterTest
+    public void stop() {
+        driver.close();
+        driver.quit();
+    }
+
+
+
+}
